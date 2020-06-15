5f6f97185fe2fd05fc26232081ad0124a3a58955
Alexey <30757221+vangogih@users.noreply.github.com>
2020-06-15 13:25:10 +0300

    Update root_isntall.sh

--- .on_new_setup/root_isntall.sh
+++ .on_new_setup/root_isntall.sh
@@ -1,12 +1,6 @@
 # sh install_zsh_and_oh-my-zsh.sh  
 apt install zsh
 chsh -s $(which zsh)
-if [$SHELL != "/bin/zsh/"]; then
-	cat <<EOF 
-	Error: zsh not default shell. Try install and set by default manually.
-	EOF
-exit 1
-fi
 sh "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # sh update_git.sh
