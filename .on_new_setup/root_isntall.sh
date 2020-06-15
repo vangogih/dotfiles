f97aa6215b5ae5767b9a3bfb439d27759d4c362b
Alexey <30757221+vangogih@users.noreply.github.com>
2020-06-15 13:34:37 +0300

    Update root_isntall.sh

--- .on_new_setup/root_isntall.sh
+++ .on_new_setup/root_isntall.sh
@@ -3,7 +3,7 @@
 # sh install_zsh_and_oh-my-zsh.sh  
 apt install zsh
 chsh -s $(which zsh)
-sh "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # sh update_git.sh
 add-apt-repository ppa:git-core/ppa -y
