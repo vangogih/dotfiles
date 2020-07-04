65478dc795a02f4753670ce4804b8bb09f4c4e06
vangogih <vangogih@yandex.ru>
2020-07-05 01:13:58 +0300

    Update sh script for auto setup: Adding downloading powerline10k theme.

--- .on_new_setup/root_isntall.sh
+++ .on_new_setup/root_isntall.sh
@@ -2,7 +2,6 @@
 
 # sh install_zsh_and_oh-my-zsh.sh  
 apt install zsh
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # sh update_git.sh
@@ -16,5 +15,10 @@ cd ..
 git init
 git remote add origin https://github.com/vangogih/dotfiles
 git checkout .
-git pull --force origin master
+git pull origin master
+git clean -f
+git pull origin master
+
+# download powerline10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
 exit 0
